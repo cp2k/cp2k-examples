994268b79aabec7707ba8c276ff094c875ce79c8
JWilhelm <jan.wilhelm@ur.de>
2024-07-24 22:40:17 +0200

    put all GW calculations for manual into cp2k-examples

---
+++ bandstructure_gw/3_3-atom_unit_cell_2d_WSe2_loose_parameters/run_noctua.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -t 10:50:00
+#SBATCH --exclusive
+#SBATCH --ntasks-per-node=16
+#SBATCH --cpus-per-task=8
+#SBATCH -N 3
+#SBATCH -J "WS2"
+#SBATCH --partition=largemem
+#SBATCH --account=hpc-prf-optgw
+
+export OMP_NUM_THREADS=8
+export OMP_PLACES=cores
+export OMP_PROC_BIND=true
+
+module reset
+module load chem/CP2K/2024.1-foss-2023b-gcc-openmpi-openblas-libgrpp
+
+srun   /pc2/groups/hpc-prf-metdyn/eprop2d1_Jan/02_compile_CP2K/54*/cp2k/exe/local/cp2k.psmp    *.inp &> cp2k.out
